e790c65023859f08757873a86bd7bd422157c7b7
Reut <michalreut0@gmail.com>
2013-06-11 05:47:34 +0200

    Wypełniona klasa wirtualna FileDownloader

--- filedownloader.h
+++ filedownloader.h
@@ -2,16 +2,52 @@
 #define FILEDOWNLOADER_H
 
 #include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QString>
 
 class FileDownloader : public QObject
 {
     Q_OBJECT
 public:
-    explicit FileDownloader(QObject *parent = 0);
+    enum ErrorType
+    {
+        NO_ERROR,
+        FILE_NOT_FOUND,
+        WRONG_RESPONSE,
+        PARTIAL_NOT_SUPPORTED,
+        UNKNOWN_ERROR
+    };
+
+    FileDownloader(QNetworkAccessManager *accessManager , QObject *parent = 0) :
+        QObject(parent)
+    {
+        this->accessManager = accessManager;
+    }
+
+    virtual bool downloadFile(QUrl url, quint64 start = 0, quint64 size = 0, quint64 chunkSize = 0) = 0;
+    virtual quint64 checkFileSize(QUrl url) = 0;
+
+    ErrorType getError()
+    {
+        return errorStatus;
+    }
+
+    virtual bool isDownloading() = 0;
+
+
+protected:
+    QNetworkAccessManager *accessManager;
+    ErrorType errorStatus = NO_ERROR;
     
 signals:
-    
+    void chunkDownloaded(QByteArray *chunkData);
+    void fileDownloaded();
+    void error(ErrorType e, QString description);
+
 public slots:
+    virtual void cancelDownload() = 0;
     
 };
 
